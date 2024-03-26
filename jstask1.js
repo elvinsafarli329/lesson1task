dbc8d8e281a31e6d95a7b76a40611081eb6e4a8a
Elvin Safarli <seferlielvin329@gmail.com>
2024-03-26 12:06:56 +0400

    javatask1

---
+++ jstask1.js
@@ -0,0 +1,32 @@
+//task 1
+// let name = prompt("Adinizi daxil edin: ");
+// console.log(name.length);
+
+//task2
+// let word = prompt("Write a sentence: ");
+// let firstWord = word.split(" ")[0];
+// console.log(firstWord.indexOf(firstWord.charAt(1)) !== -1);
+
+//task3
+// let word= prompt("Write a sentence: ");
+// let firstWord = word.split(" ")[0];
+// console.log(firstWord.startsWith(firstWord.charAt(1)));
+
+//task4
+// let word= prompt("Write a sentence: ");
+// let firstWord = word.split(" ")[0];
+// console.log(firstWord.endsWith(firstWord.charAt(1)));
+
+//task5
+// let word = prompt("Write a word: ");
+// let firstWord = sentence.split(" ")[0];
+// console.log(firstWord[1]);
+
+//task6
+// let word= prompt("Write a word: ");
+// if (word === word.toUpperCase()) {
+//     console.log("true");
+// }
+// else {
+//     console.log("false");
+// }
