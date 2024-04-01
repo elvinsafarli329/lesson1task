847028f89cc6c2a51454cdd9b1200c24faf536dc
Elvin Safarli <seferlielvin329@gmail.com>
2024-04-01 10:47:10 +0400

    Jstask3

---
+++ jstask3.js
@@ -0,0 +1,72 @@
+
+//1.Object yarat
+// let candidates = {
+//     name: "unknown",
+//     height: "150",
+//     weight: "80",
+//     age: "20"
+// }
+// console.log(candidates);
+
+
+
+// task 2, 3, 4
+// let students = [
+//     {
+//         Id: 1,
+//         name: "Asad",
+//         surname: "Asadov",
+//         age: 30,
+//         hobbies: ["chess", "futball", "music"],
+//         score: 100
+//     },
+//     {
+//         Id: 2,
+//         name: "Elman",
+//         surname: "Hakimov",
+//         age: 35,
+//         hobbies: ["jugging", "reading novels", "drawing"],
+//         score: 80
+//     },
+//     {
+//         Id: 3,
+//         name: "Fuad",
+//         surname: "Safaraliyev",
+//         age: 38,
+//         hobbies: ["writing poems", "playing piano", "fighting"],
+//         score: 90,
+//     },
+//     {
+//         Id: 4,
+//         name: "Turab",
+//         surname: "Imanov",
+//         age: 38,
+//         hobbies: ["writing poems", "playing piano", "fighting"],
+//         score: 40,
+//     },
+//     {
+//         Id: 5,
+//         name: "Yusuf",
+//         surname: "Mammadov",
+//         age: 38,
+//         hobbies: ["writing poems", "playing piano", "fighting"],
+//         score: 95,
+//     },
+// ]
+
+//task4
+// let scores = students.map((a) => a.score);
+// let average = 0;
+// scores.forEach(num => {
+//     average += num;
+// }
+// )
+// console.log(`The average score of all students is ${average / scores.length}`);
+ 
+
+// task5
+// students.map((student) => {
+//     if (student.score < 90) {
+//     alert(`${student.name} ${student.surname}`);
+//     }
+// }) ;
