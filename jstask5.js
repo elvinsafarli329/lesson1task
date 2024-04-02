a8c6be15d242c14f4cf4bc1336b3ffbd91240b44
Elvin Safarli <seferlielvin329@gmail.com>
2024-04-03 01:05:26 +0400

    jstask5

---
+++ jstask5.js
@@ -0,0 +1,59 @@
+//task1
+// let products = [
+//     {
+//         id: 1,
+//         name: "phone 1",
+//         price: 1200,
+//     },
+//     {
+//         id: 2,
+//         name: "phone 2",
+//         price: 900,
+//     },
+//     {
+//         id: 3,
+//         name: "phone 3",
+//         price: 800,
+//     },
+//     {
+//         id: 4,
+//         name: "phone 4",
+//         price: 1300,
+//     },
+//     {
+//         id: 5,
+//         name: "phone 5",
+//         price: 1450,
+//     },
+//     {
+//         id: 6,
+//         name: "phone 6",
+//         price: 150,
+//     },
+// ]
+
+
+
+
+// let product = products.sort((a, b) => a.price - b.price);
+// product.map(a => {
+//     console.log(a.name);
+// });
+
+//task2
+// let emptyArray = [];
+// let  sum = 0;
+
+// let a = Math.floor(Math.random()*10);
+// let b = Math.floor(Math.random()*100);
+// let c = Math.floor(Math.random()*20);
+// let d = Math.floor(Math.random()*30);
+// let e = Math.floor(Math.random()*50);
+
+// emptyArray.push(a, b, c, d, e)
+// emptyArray.map((adding) => {
+//     for (i=adding; i <= adding; i++) {
+//         sum += i;
+//     }
+// } )
+// console.log(sum);
