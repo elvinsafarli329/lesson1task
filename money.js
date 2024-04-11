3342516366772e3164100acc3ff08f5aef9930ad
Elvin Safarli <seferlielvin329@gmail.com>
2024-04-11 14:04:37 +0400

    dynamictask.js

--- money.js
+++ money.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById("btn");
 const tbody = document.getElementById("tbody");
 
+
 btn.addEventListener("click", () => {
     const tr = document.createElement("tr");
 
@@ -12,6 +13,7 @@ btn.addEventListener("click", () => {
     const agetd = document.createElement("td");
     const buttonstd1 = document.createElement("td");
     const buttonstd2 = document.createElement("td");
+    const buttonstd3 = document.createElement("td");
 
     const name = document.createElement("input");
     name.placeholder = "Adiniz...";
@@ -36,12 +38,33 @@ btn.addEventListener("click", () => {
         age.style.backgroundColor = "white";
         age.style.pointerEvents = "none";
 
-        buttons1.textContent = "duzelis et";
-        buttons1.style.backgroundColor = "yellow";
-        
+        const p1 = document.createElement("p");
+        p1.textContent = ad;
+        const p2 = document.createElement("p");
+        p2.textContent = soyad;
+        const p3 = document.createElement("p");
+        p3.textContent = yas;
+        const update=document.querySelector("#update");
+        update.style.display="inline-block";
+        buttons1.style.display="none"
+
+        name.remove();
+        nametd.append(p1);
+
+        surname.remove();
+        surnametd.append(p2);
+
+        age.remove();
+        agetd.append(p3);
+
     });
     const buttons2 = document.createElement("button");
     buttons2.textContent = "Sil";
+    
+    const buttons3 = document.createElement("button");
+    buttons3.id="update";
+    buttons3.textContent = "duzelis et";
+    buttons3.style.display = "none";
 
     const siralabel = document.createElement("label");
     const namelabel = document.createElement("label");
@@ -54,8 +77,9 @@ btn.addEventListener("click", () => {
     agetd.append(age, agelabel);
     buttonstd1.append(buttons1);
     buttonstd2.append(buttons2);
+    buttonstd3.append(buttons3);
 
-    tr.append(sira, nametd, surnametd, agetd, buttons1, buttons2);
+    tr.append(sira, nametd, surnametd, agetd, buttons1, buttons2, buttons3);
 
     tbody.appendChild(tr);
 })
